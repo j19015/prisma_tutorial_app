8b5ea53eb8ed64a260c09a3688f0a9469364d24a
j19015 <j19015@sangi.jp>
2024-03-16 18:17:28 +0900

    feat Footer

---
+++ src/app/components/Footer/Footer.tsx
@@ -0,0 +1,11 @@
+'use client'
+
+import { Layout } from 'antd';
+
+export default function Footer() {
+    return (
+        <Layout.Footer>
+            <p style={{textAlign: 'center'}}>© 2024 Your Company</p>
+        </Layout.Footer>
+    );
+}
