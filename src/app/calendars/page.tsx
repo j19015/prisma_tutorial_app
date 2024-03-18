5d1f40a5b48c19698513deedf70c8f6e1c543af6
j19015 <j19015@sangi.jp>
2024-03-18 23:25:00 +0900

    feat modalを実装

--- src/app/calendars/page.tsx
+++ src/app/calendars/page.tsx
@@ -10,23 +10,23 @@ import interactionPlugin from "@fullcalendar/interaction";
 //日本語対応のためのインポート
 import jaLocale from "@fullcalendar/core/locales/ja";　//追加
 
-import { Card,Form,Row,Col,Input,Button,DatePicker,Select } from 'antd';
+import { Card,Form,Row,Col,Input,Button,DatePicker,Select,Modal } from 'antd';
 
 type Event = {
     title: string;
     start: Date;
-    end?: Date;
-    description?: string;
-    backgroundColor?: string;
-    borderColor?: string;
+    end: Date;
+    description: string;
+    backgroundColor: string;
+    borderColor: string;
 }
 
 
 
 export default function Home() {
     const [form] = Form.useForm();
-
     const [dataSource, setDataSource] = useState<Event[]>([]);
+    const [modalVisible, setModalVisible] = useState(false);
 
     useEffect(() => {
         const fetchCalendars = async () => {
@@ -50,6 +50,7 @@ export default function Home() {
         const calendars = await response.json();
         setDataSource(calendars);
         form.resetFields();
+        setModalVisible(false); // モーダルを閉じる
     }
 
     const centeredStyle: React.CSSProperties = {
@@ -58,76 +59,92 @@ export default function Home() {
         alignItems: 'center',
         marginTop: '50px',
     };
+
+    const showModal = () => {
+        setModalVisible(true);
+    };
     
 
     return (
-        <div style={centeredStyle}>
-            <Form onFinish={handleSaveClick} form={form} style={{width: '30%'}}>
-                <Row gutter={16}>
-                    <Col span={16}>
-                        <Form.Item 
-                            name="title"
-                            label="Title"
-                            rules={[{required: true, message:"titleを入力してください。"}]}>
-                            <Input placeholder="title"/>
-                        </Form.Item>
-                    </Col>
-                    <Col span={16}>
-                        <Form.Item 
-                            name="start"
-                            label="Start"
-                            rules={[{required: true, message:"開始日を入力してください"}]}>
-                            <DatePicker placeholder="start" name="start" />
-                        </Form.Item>
-                    </Col>
-                    <Col span={16}>
-                        <Form.Item 
-                            name="end"
-                            label="End"
-                            rules={[{required: true, message:"終了日を入力してください"}]}>
-                            <DatePicker placeholder="end" name="end" />
-                        </Form.Item>
-                    </Col>
-                    <Col span={16}>
-                        <Form.Item
-                            name="backgroundColor"
-                            label="Background Color"
-                            rules={[{required: true, message: "背景色を選択してください。"}]}>
-                            <Select placeholder="背景色を選択してください。" >
-                                <Select.Option value="red">Red</Select.Option>
-                                <Select.Option value="blue">Blue</Select.Option>
-                                <Select.Option value="green">Green</Select.Option>
-                            </Select>
-                        </Form.Item>
-                    </Col>
-                    <Col span={16}>
-                        <Form.Item
-                            name="borderColor"
-                            label="Border Color"
-                            rules={[{required: true, message: "枠線色を選択してください。"}]}>
-                            <Select placeholder="枠線色を選択してください。" >
-                                <Select.Option value="red">Red</Select.Option>
-                                <Select.Option value="blue">Blue</Select.Option>
-                                <Select.Option value="green">Green</Select.Option>
-                            </Select>
-                        </Form.Item>
-                    </Col>
-                    <Col span={16}>
-                        <Form.Item>
-                            <Button type="primary" htmlType='submit'>Save</Button>
-                        </Form.Item>
-                    </Col>
-                </Row>
-            </Form>
+        <>
+            <div style={centeredStyle}>
+                <Button type="primary" onClick={showModal}>Add Calendar</Button>
+            </div>
+            <div style={centeredStyle}>
+                <Modal 
+                    title="Add Calendar"
+                    open={modalVisible}
+                    onCancel={() => setModalVisible(false)}
+                    footer={null}
+                >
+                    <Form onFinish={handleSaveClick} form={form} style={{marginTop: '30px'}}>
+                        <Row gutter={16}>
+                            <Col span={20}>
+                                <Form.Item 
+                                    name="title"
+                                    label="Title"
+                                    rules={[{required: true, message:"titleを入力してください。"}]}>
+                                    <Input placeholder="title"/>
+                                </Form.Item>
+                            </Col>
+                            <Col span={20} style={{marginTop: '20px'}}>
+                                <Form.Item 
+                                    name="start"
+                                    label="Start"
+                                    rules={[{required: true, message:"開始日を入力してください"}]}>
+                                    <DatePicker placeholder="start" name="start" />
+                                </Form.Item>
+                            </Col>
+                            <Col span={20} style={{marginTop: '20px'}}>
+                                <Form.Item 
+                                    name="end"
+                                    label="End"
+                                    rules={[{required: true, message:"終了日を入力してください"}]}>
+                                    <DatePicker placeholder="end" name="end" />
+                                </Form.Item>
+                            </Col>
+                            <Col span={20} style={{marginTop: '20px'}}>
+                                <Form.Item
+                                    name="backgroundColor"
+                                    label="Background Color"
+                                    rules={[{required: true, message: "背景色を選択してください。"}]}>
+                                    <Select placeholder="select a color" >
+                                        <Select.Option value="red">Red</Select.Option>
+                                        <Select.Option value="blue">Blue</Select.Option>
+                                        <Select.Option value="green">Green</Select.Option>
+                                    </Select>
+                                </Form.Item>
+                            </Col>
+                            <Col span={20} style={{marginTop: '20px'}}>
+                                <Form.Item
+                                    name="borderColor"
+                                    label="Border Color"
+                                    rules={[{required: true, message: "枠線色を選択してください。"}]}>
+                                    <Select placeholder="select a color" >
+                                        <Select.Option value="red">Red</Select.Option>
+                                        <Select.Option value="blue">Blue</Select.Option>
+                                        <Select.Option value="green">Green</Select.Option>
+                                    </Select>
+                                </Form.Item>
+                            </Col>
+                            <Col span={16}>
+                                <Form.Item>
+                                    <Button type="primary" htmlType='submit'>Save</Button>
+                                </Form.Item>
+                            </Col>
+                        </Row>
+                    </Form>
+                </Modal>
 
-            <Card title= 'Calendars' style={{width: 1000}}>
-                <FullCalendar
-                locale={jaLocale}
-                plugins={[dayGridPlugin,interactionPlugin]}
-                initialView="dayGridMonth"
-                events={dataSource}
-                />
-            </Card>
-        </div>
+                <Card title= 'Calendars' style={{width: 1000}}>
+                    <FullCalendar
+                    locale={jaLocale}
+                    plugins={[dayGridPlugin,interactionPlugin]}
+                    initialView="dayGridMonth"
+                    events={dataSource}
+                    />
+                </Card>
+            </div>
+        </>
     )
 }
